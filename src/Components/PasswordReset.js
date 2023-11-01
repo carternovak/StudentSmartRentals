da35c088e327a90bddc25f767c8440983c7dc722
dashack <dashack@iu.edu>
2023-11-01 13:03:24 -0400

    Updated PasswordReset CSS to be consistent

--- src/Components/PasswordReset.js
+++ src/Components/PasswordReset.js
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { Form, Alert } from "react-bootstrap";
 import { Button } from "react-bootstrap";
 import { useUserAuth } from "../context/UserAuthContext";
+import "../css/Login.css";
 
 const PasswordReset = () => {
   const [email, setEmail] = useState("");
@@ -23,24 +24,26 @@ const PasswordReset = () => {
 
   return (
     <>
-      <div className="p-4 box">
-        <h2 className="mb-5 text-center">Forgot Password</h2>
-        {error && <Alert variant="danger">{error}</Alert>}
-        <Form onSubmit={handleSubmit}>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Control
-              type="email"
-              placeholder="Email address"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </Form.Group>
+      <div className="login_container">
+        <div className="p-4 box login">
+          <h2 className="mb-5 text-center">Forgot Password</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Control
+                type="email"
+                placeholder="Email address"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Form.Group>
 
-          <div className="d-grid gap-2">
-            <Button variant="primary" type="Submit">
-              Reset Password
-            </Button>
-          </div>
-        </Form>
+            <div className="d-grid gap-2">
+              <Button variant="primary" type="Submit">
+                Reset Password
+              </Button>
+            </div>
+          </Form>
+        </div>
       </div>
     </>
   );
